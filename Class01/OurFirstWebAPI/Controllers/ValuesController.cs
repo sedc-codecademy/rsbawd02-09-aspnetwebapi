ca3bc910025efd0735fb9d3895da45ad7789cbaa
Almir Vuk <almir.vuk@outlook.com>
2024-09-10 20:06:52 +0200

    Class01 code added.

---
+++ Class01/OurFirstWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController() {}
+
+        [HttpGet]
+        [Route("helloWorld")]
+        public string HelloWorld()
+        {
+            return "Hello World from Qinshift Academy";
+        }
+    }
+}
---
+++ Class01/OurFirstWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using OurFirstWebAPI.Models;
+
+namespace OurFirstWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("getByCityName")]
+        public WeatherForecast GetByCityName(string city)
+        {
+            WeatherForecast weatherForecast = new WeatherForecast();
+
+            weatherForecast.TemperatureC = 35;
+            weatherForecast.Summary = $"Currently Hot in {city}";
+            weatherForecast.Date = DateOnly.FromDateTime(DateTime.Now);
+
+            return weatherForecast;
+        }
+    }
+}

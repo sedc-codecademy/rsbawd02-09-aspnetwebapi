51c871a27bcc97cb34a2f66e8e6fec6b7f4dbb7e
Almir Vuk <almir.vuk@outlook.com>
2024-10-03 20:47:29 +0200

    fix

--- Class07/UserNotesAPI/Domain/NoteTakingDbContext.cs
+++ Class07/UserNotesAPI/Domain/NoteTakingDbContext.cs
@@ -21,7 +21,7 @@ public partial class NoteTakingDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=avdemoacademy.database.windows.net;Database=NoteTakingDB;User Id=toor;Password=12345678Abc");
+        => optionsBuilder.UseSqlServer("YOUR CONNECTION STRING");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
